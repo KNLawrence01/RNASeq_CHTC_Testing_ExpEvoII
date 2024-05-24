9fdc4c7b88ac99c04edf8c7c1a483bd83d08df72
Kadee Nicole Lawrence <108096710+KNLawrence01@users.noreply.github.com>
2024-05-24 13:58:02 -0500

    Update bwaMEM.sh

--- bwaMEM.sh
+++ bwaMEM.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-bwa index $1
-bwa mem -M -t 8 $1 $2_1P.fq.gz $2_2P.fq.gz > $2.sam
+bwa index MtbNCBIH37RV.fa
+bwa mem -M -t 8 MtbNCBIH37RV.fa 3151_17_S11_1P.fq.gz 3151_17_S11_2P.fq.gz > 3151_17_S11.sam
