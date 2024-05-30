65ffaa500378f86909d7df5a2d5f60a0542d1552
Kadee Nicole Lawrence <108096710+KNLawrence01@users.noreply.github.com>
2024-05-30 14:04:19 -0500

    Update bwaMEM_1.sh

--- bwaMEM_1.sh
+++ bwaMEM_1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-bwa index MtbNCBIH37RV.fa
-bwa mem -M -t 8 MtbNCBIH37RV.fa 3151_17_S11_1P.fq.gz 3151_17_S11_2P.fq.gz > 3151_17_S11.sam
+bwa index MtbNCBIH37Rv.fa
+bwa mem -M -t 8 MtbNCBIH37Rv.fa 3151_17_S11_1P.fq.gz 3151_17_S11_2P.fq.gz > 3151_17_S11.sam
