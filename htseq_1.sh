7cf84f5fc3f00625c789bf6dff2cb6030d8d5f44
Kadee Nicole Lawrence <108096710+KNLawrence01@users.noreply.github.com>
2024-06-05 14:23:50 -0500

    Update htseq_1.sh

--- htseq_1.sh
+++ htseq_1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 -m HTSeq.scripts.count -f bam -r pos -t gene -i gene_name --nonunique none -s reverse 3151_17_S11.sort.bam MtbNCBIH37Rv.gtf > 3151_17_S11_HTSeqCounts_s_rev.txt
+python3 -m HTSeq.scripts.count -f bam -r pos -t gene -i gene_name --nonunique none -s reverse 3151_17_S11.sort.bam MtbNCBIH37Rv.gtf > 3151_17_S11_HTSeqCounts.txt
