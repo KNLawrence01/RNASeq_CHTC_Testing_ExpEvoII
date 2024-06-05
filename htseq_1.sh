fc2c5e92a6722004327523dd688da4ffd186bfd9
Kadee Nicole Lawrence <108096710+KNLawrence01@users.noreply.github.com>
2024-06-05 14:10:00 -0500

    Update htseq_1.sh
    Wanted index

--- htseq_1.sh
+++ htseq_1.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python3 -m HTSeq.scripts.count -f bam -r pos -t gene -i gene_name --nonunique none -s reverse 3151_17_S11.sort.bam MtbNCBIH37Rv.gff
+samtools index 3151_17_S11.sort.bam
+python3 -m HTSeq.scripts.count -f bam -r pos -t gene -i gene_name --nonunique none -s reverse 3151_17_S11.sort.bam MtbNCBIH37Rv.gtf
